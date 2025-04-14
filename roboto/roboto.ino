532fcab621403eac3b8478ba497904a0e99080b1
luhj <luhj@itu.dk>
2025-04-14 15:56:49 +0200

    update roboto

--- roboto/roboto.ino
+++ roboto/roboto.ino
@@ -6,6 +6,7 @@ int S = 10;
 Servo servo1;
 Servo servo2;
 Servo servo3;
+Servo neck;
 
 Adafruit_NeoPixel leftEye(64, 2, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel rightEye(64, 3, NEO_GRB + NEO_KHZ800);
@@ -18,22 +19,26 @@ void setup() {
   servo1.attach(9);
   servo2.attach(10);
   servo3.attach(11);
+  neck.attach(12);
   servo1.write(180);
   servo2.write(180);
   servo3.write(180);
+  neck.write(90);
 }
 
 void loop() {
-  for (int i = 0; i < 8; i++)
+  look_around();
+  happy();
+  for (int i = 0; i < 5; i++)
   {
-    heart();
+    heart_eyes();
     delay(30 * S);
     clear();
     show();
     delay(10 * S);
   }
   
-  happy();
+  angry();
   sad();
 }
 
@@ -54,45 +59,45 @@ void show() {
 void bigEyes(int dir) {
   const int eye0[8][8] = {
     {0,0,0,0,0,0,0,0},
-    {0,0,2,2,2,2,0,0},
-    {0,2,0,0,0,0,2,0},
-    {2,0,0,1,1,0,0,2},
-    {2,0,1,1,1,1,0,2},
-    {0,2,1,1,1,1,2,0},
-    {0,0,2,2,2,2,0,0},
+    {0,2,2,2,2,2,2,0},
+    {0,2,2,2,2,2,2,0},
+    {0,2,2,1,1,2,2,0},
+    {0,2,2,1,1,2,2,0},
+    {0,2,2,2,2,2,2,0},
+    {0,2,2,2,2,2,2,0},
     {0,0,0,0,0,0,0,0}
   };
 
   const int eye1[8][8] = {
     {0,0,0,0,0,0,0,0},
-    {0,0,2,2,2,2,0,0},
-    {0,2,0,0,0,1,2,0},
-    {2,0,0,0,1,1,1,2},
-    {2,0,0,0,1,1,1,2},
-    {0,2,0,0,0,1,2,0},
-    {0,0,2,2,2,2,0,0},
+    {0,2,2,2,2,2,2,0},
+    {0,2,2,2,2,2,2,0},
+    {0,2,2,2,1,1,2,0},
+    {0,2,2,2,1,1,2,0},
+    {0,2,2,2,2,2,2,0},
+    {0,2,2,2,2,2,2,0},
     {0,0,0,0,0,0,0,0}
   };
 
   const int eye2[8][8] = {
     {0,0,0,0,0,0,0,0},
-    {0,0,2,2,2,2,0,0},
-    {0,2,1,1,1,1,2,0},
-    {2,0,1,1,1,1,0,2},
-    {2,0,0,1,1,0,0,2},
-    {0,2,0,0,0,0,2,0},
-    {0,0,2,2,2,2,0,0},
+    {0,2,2,2,2,2,2,0},
+    {0,2,2,2,2,2,2,0},
+    {0,2,1,1,2,2,2,0},
+    {0,2,1,1,2,2,2,0},
+    {0,2,2,2,2,2,2,0},
+    {0,2,2,2,2,2,2,0},
     {0,0,0,0,0,0,0,0}
   };
 
   const int eye3[8][8] = {
     {0,0,0,0,0,0,0,0},
-    {0,0,2,2,2,2,0,0},
-    {0,2,1,0,0,0,2,0},
-    {2,1,1,1,0,0,0,2},
-    {2,1,1,1,0,0,0,2},
-    {0,2,1,0,0,0,2,0},
-    {0,0,2,2,2,2,0,0},
+    {0,2,2,2,2,2,2,0},
+    {0,2,2,2,2,2,2,0},
+    {0,2,2,2,2,2,2,0},
+    {0,2,2,1,1,2,2,0},
+    {0,2,2,1,1,2,2,0},
+    {0,2,2,2,2,2,2,0},
     {0,0,0,0,0,0,0,0}
   };
 
@@ -111,12 +116,12 @@ void bigEyes(int dir) {
   for (int i = 0; i < 8; i++) {
     for (int j = 0; j < 8; j++) {
       if (selectedEye[j][i] == 1) {
-        leftEye.setPixelColor(getPixelIndex(i, j), leftEye.Color(255, 0, 255));
-        rightEye.setPixelColor(getPixelIndex(7 - i, j), rightEye.Color(255, 0, 255));
+        leftEye.setPixelColor(getPixelIndex(i, j), leftEye.Color(0, 255, 0));
+        rightEye.setPixelColor(getPixelIndex(i, j), rightEye.Color(0, 255, 0));
       }
-      else if (selectedEye[j][i] == 2) {
-        leftEye.setPixelColor(getPixelIndex(i, j), leftEye.Color(255, 255, 255));
-        rightEye.setPixelColor(getPixelIndex(7 - i, j), rightEye.Color(255, 255, 255));
+      if (selectedEye[j][i] == 2) {
+        leftEye.setPixelColor(getPixelIndex(i, j), leftEye.Color(0, 0, 255));
+        rightEye.setPixelColor(getPixelIndex(i, j), rightEye.Color(0, 0, 255));
       }
     }
   }
@@ -127,14 +132,14 @@ void bigEyes(int dir) {
 
 void blink() {
   int eye[8][8] = {
-    {0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 1, 1, 1, 1, 1, 1, 0},
-    {0, 1, 1, 1, 1, 1, 1, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0}
+    {0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0},
+    {0,0,1,1,1,1,0,0},
+    {0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0},
   };
 
   leftEye.clear();
@@ -142,8 +147,8 @@ void blink() {
   for (int i = 0; i < 8; i++) {
     for (int j = 0; j < 8; j++) {
       if (eye[j][i]) {
-        leftEye.setPixelColor(getPixelIndex(i, j), leftEye.Color(255, 0, 255));
-        rightEye.setPixelColor(getPixelIndex(7-i, j), rightEye.Color(255, 0, 255));
+        leftEye.setPixelColor(getPixelIndex(i, j), leftEye.Color(0, 0, 255));
+        rightEye.setPixelColor(getPixelIndex(7-i, j), rightEye.Color(0, 0, 255));
       }
     }
   }
@@ -152,21 +157,20 @@ void blink() {
 }
 
 
-void heart() {
+void heart_eyes() {
   int eye[8][8] = {
-    {0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0, 2, 2, 0, 2, 2, 0},
-    {0, 2, 1, 1, 2, 1, 1, 2},
-    {0, 2, 1, 1, 1, 1, 1, 2},
-    {0, 2, 1, 1, 1, 1, 1, 2},
-    {0, 0, 2, 1, 1, 1, 2, 0},
-    {0, 0, 0, 2, 1, 2, 0, 0},
-    {0, 0, 0, 0, 2, 0, 0, 0},
+    {0,2,2,0,0,2,2,0},
+    {2,1,1,2,2,1,1,2},
+    {2,1,1,1,1,1,1,2},
+    {2,1,1,1,1,1,1,2},
+    {2,1,1,1,1,1,1,2},
+    {0,2,1,1,1,1,2,0},
+    {0,0,2,1,1,2,0,0},
+    {0,0,0,2,2,0,0,0},
   };
 
 
-  leftEye.clear();
-  rightEye.clear();
+  clear();
   for (int i = 0; i < 8; i++) {
     for (int j = 0; j < 8; j++) {
       if (eye[j][i] == 1) {
@@ -179,20 +183,90 @@ void heart() {
       }
     }
   }
-  leftEye.show();
-  rightEye.show();
+  show();
+}
+
+void looking_eyes() {
+  int eye[8][8] = {
+    {0,0,0,0,0,0,0,0},
+    {1,0,0,0,0,0,0,1},
+    {1,1,1,1,1,1,1,1},
+    {0,0,0,0,0,1,1,0},
+    {0,0,0,0,0,1,1,0},
+    {0,0,0,0,0,1,1,0},
+    {0,0,0,0,0,1,1,0},
+    {0,0,0,0,0,1,1,0}
+  };
+  clear();
+  for (int i = 0; i < 8; i++) {
+    for (int j = 0; j < 8; j++) {
+      if (eye[j][i] == 1) {
+        leftEye.setPixelColor(getPixelIndex(i, j), leftEye.Color(0, 0, 255));
+        rightEye.setPixelColor(getPixelIndex(7-i, j), rightEye.Color(0, 0, 255));
+      }
+    }
+  }
+  show();
+}
+
+
+void happy_eyes() {
+  int eye[8][8] = {
+    {0,0,0,0,0,0,0,0},
+    {0,0,1,1,1,1,0,0},
+    {0,1,0,0,0,0,1,0},
+    {0,1,0,0,0,0,1,0},
+    {0,1,0,0,0,0,1,0},
+    {0,1,0,0,0,0,1,0},
+    {0,1,0,0,0,0,1,0},
+    {0,1,0,0,0,0,1,0},
+  };
+  clear();
+  for (int i = 0; i < 8; i++) {
+    for (int j = 0; j < 8; j++) {
+      if (eye[j][i] == 1) {
+        leftEye.setPixelColor(getPixelIndex(i, j), leftEye.Color(0, 0, 255));
+        rightEye.setPixelColor(getPixelIndex(7-i, j), rightEye.Color(0, 0, 255));
+      }
+    }
+  }
+  show();
+}
+
+
+void angry_eyes() {
+  int eye[8][8] = {
+    {0,0,0,0,0,0,0,0},
+    {0,1,0,0,0,0,0,0},
+    {0,1,1,0,0,0,0,0},
+    {0,1,1,1,0,0,0,0},
+    {0,1,1,1,1,0,0,0},
+    {0,1,1,1,1,1,0,0},
+    {0,1,1,1,1,1,1,0},
+    {0,0,0,0,0,0,0,0},
+  };
+  clear();
+  for (int i = 0; i < 8; i++) {
+    for (int j = 0; j < 8; j++) {
+      if (eye[j][i] == 1) {
+        leftEye.setPixelColor(getPixelIndex(i, j), leftEye.Color(255, 0, 0));
+        rightEye.setPixelColor(getPixelIndex(7-i, j), rightEye.Color(255, 0, 0));
+      }
+    }
+  }
+  show();
 }
 
 void happy() {
   bigEyes(0);
-  delay(5 * S);
+  delay(50 * S);
   bigEyes(1);
-  delay(5 * S);
+  delay(50 * S);
   bigEyes(2); 
-  delay(5 * S);
+  delay(50 * S);
   bigEyes(3);
-  delay(5 * S);
-  bigEyes(0);
+  delay(50 * S);
+  happy_eyes();
   servo1.write(0);
   delay(150 * S);
   servo1.write(180);
@@ -205,12 +279,77 @@ void happy() {
   delay(10 * S);
   bigEyes(0);
   servo2.write(180);
-  delay(200 * S);
+  delay(50 * S);
+  happy_eyes();
+  delay(150 * S);
 }
 
 void sad() {
-  bigEyes(0);
+  bigEyes(3);
   servo2.write(0);
   delay(300 * S);
   servo2.write(180);
 }
+
+void look_around() {
+  servo1.write(180);
+  servo2.write(180);
+  servo3.write(180);
+  looking_eyes();
+  neck.write(30);
+  delay(150 * S);
+  blink();
+  delay(10 * S);
+  looking_eyes();
+  neck.write(150);
+  blink();
+  delay(10 * S);
+  looking_eyes();
+  delay(150 * S);
+  neck.write(90);
+  delay(200 * S);
+
+}
+
+void angry() {
+  angry_eyes();
+  for (int i = 0; i < 5; i++) {
+    servo2.write(40);
+    delay(20 * S);
+    servo2.write(0);
+    delay(20 * S);
+  }
+}
+
+uint8_t HEART_OUTSIDE[8] = {
+  0b01100110,
+  0b10011001,
+  0b10000001,
+  0b10000001,
+  0b10000001,
+  0b01000010,
+  0b00100100,
+  0b00011000
+};
+
+uint8_t HEART_INSIDE[8] = {
+  0b00000000,
+  0b01100110,
+  0b01111110,
+  0b01111110,
+  0b01111110,
+  0b00111100,
+  0b00011000,
+  0b00000000
+};
+
+uint8_t LOOKING_LEFT[8] = {
+  0b00000000,
+  0b10000001,
+  0b11111111,
+  0b00000110,
+  0b00000110,
+  0b00000110,
+  0b00000110,
+  0b00000110,
+};
