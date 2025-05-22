262a8dc2b17d2f2802e17d529186bdef437d5317
yuzhuliu2025 <liuyuzhu2023@163.com>
2025-05-22 17:38:02 +0200

    sensor i是农田投sensitive

--- sim/src/Sensor.h
+++ sim/src/Sensor.h
@@ -6,7 +6,7 @@ bool tapped = false;
 bool sensorLoop(uint8_t* state, unsigned long currentMillis) {
     if (DEBUG && currentMillis % 1000 == 600) Serial.println("sensors");
     // TODO: should this only work if state is <= 19?
-    if (!(*state == IDLE || *state == ONETAP || *state == TWOTAP) || currentMillis - lastSensorLoop < 20) return false;
+    if (currentMillis - lastSensorLoop < 20) return false;
     if ((*state == ONETAP || *state == TWOTAP) && currentMillis - lastTap > 1000) *state = GAMESTART;
     bool sensorState = digitalRead(2);
     if (*state != TWOTAP && sensorState == HIGH && lastSensorState == LOW) {
